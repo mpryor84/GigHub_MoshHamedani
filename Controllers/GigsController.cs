68ab5338dca4282c5461b478fb17fa9e778e2ec0
Pryor, Matt (Health) <Matt.Pryor@sa.gov.au>
2020-03-13 16:27:24 +1030

    Implement Server Validation

--- Controllers/GigsController.cs
+++ Controllers/GigsController.cs
@@ -30,6 +30,12 @@ namespace GigHub.Controllers
         [HttpPost]
         public ActionResult Create(GigFormViewModel viewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("Create", viewModel);
+            }
+
             var artistId = User.Identity.GetUserId();
             var artist = _context.Users.Single(u => u.Id == artistId);
             var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
@@ -37,7 +43,7 @@ namespace GigHub.Controllers
             var gig = new Gig
             {
                 Artist = artist,
-                DateTime = viewModel.DateTime,
+                DateTime = viewModel.GetDateTime(),
                 Genre = genre,
                 Venue = viewModel.Venue
 
--- ViewModels/GigFormViewModel.cs
+++ ViewModels/GigFormViewModel.cs
@@ -1,23 +1,33 @@
-﻿using System;
-using GigHub.Models;
+﻿using GigHub.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+
+        [Required]
         public int Genre { get; set; }
+
+        [Required]
         public IEnumerable<Genre> Genres { get; set; }
 
-        public DateTime DateTime
+        public DateTime GetDateTime()
         {
-            get
-            {
-                return DateTime.Parse(string.Format("{0} {1}", Date, Time));
-            }
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
         }
+
     }
 }
